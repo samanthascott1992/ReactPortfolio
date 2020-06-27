0ce42795a84241729a6acc3ead5654aafc55a3dd
Samantha Scott <samanthascott1992@gmail.com>
2020-06-27 15:43:11 -0600

    updating with project 3

--- src/App.js
+++ src/App.js
@@ -1,64 +1,77 @@
 import React from 'react';
 import boozyFood from './images/BoozyFood.PNG';
-import sellThings from './images/sellyostuff.PNG'
+import sellThings from './images/sellyostuff.PNG';
+import passwordgenerator from './images/PasswordGenerator.PNG';
+import weatherdashboard from './images/weatherdashboard.PNG'
+import linkedin from './images/linkedin.png';
+import github from './images/Github.png';
+import twitter from './images/twitter.png';
+import cohortcorner from './images/cohortcorner.png';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <nav>
+               <a className="btn btn-outline-light btn-social mx-1" href="https://twitter.com/Samanth25431418">
+                <img className="nav-icon" src={twitter} alt="twitter"/>
+              </a>
+              <a className="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/samantha-scott1992/">
+                <img className="nav-icon" src={linkedin} alt="linkedin"/>
+              </a>
+              <a className="btn btn-outline-light btn-social mx-1 " href="https://github.com/samanthascott1992" title="GitHub">
+                <img className="nav-icon" src={github} alt="github" />
+              </a>
+      </nav>
       <header className="App-header">
        
         <p> Samantha Scott
-          <code>  </code>
+          <br />
+          samanthascott1992@gmail.com | 540-623-0694 |Aurora, Colorado
         </p>
-        <p>
-         samanthascott1992@gmail.com                  
-        </p>
-        <p>
-        540-623-0694
-        </p>
+        <p className="aboutMe">As a University of Denver Coding Boot Camp student I have been acquiring the skills necessary to be a full stack web developer. 
+        <br />Actively participating in class and projects has helped me learn that there are difficult obstacles that will have to be overcome, and often they are not as hard as they seem.
+        </p>               
       </header>
-        <p>As a University of Denver Coding Boot Camp student I have been acquiring the skills necessary to be a full stack web developer. Actively participating in class and projects has helped me learn that there are difficult obstacles that will have to be overcome, and often they are not as hard as they seem.
+      <img src={cohortcorner} className="Project-Three portfolioImg" alt="project3" />
+      {/* Place Image HERE */}
+        <p>CohortCorner is a resource for new developers to be able to expand their knowlegde.   
+          <br />This application allows users to view and add links for subjects that have been added to the database.  
+        
         </p>
-
+      
          <img src={boozyFood} className="Project-One portfolioImg" alt="project1" />
       {/* Place Image HERE */}
-        <p>BoozyFood was a collaborative effort with two other classmates. When you search for an ingredient, then up to five recipes generate. When you click the Random Drink! button, you will be presented with a drink image, ingredients, and instructions for how to make it. 
+        <p>BoozyFood was a collaborative effort with two other classmates. When you search for an ingredient, then up to five recipes generate. 
+          <br />When you click the Random Drink! button, you will be presented with a drink image, ingredients, and instructions for how to make it. 
+        
         </p>
         <img src={sellThings} className="Project-Two portfolioImg" alt="project2" />
       {/* Place Image HERE */}
-        <p> SellYoStuff is a work that was created with two classmates, Tyler B and Zach. When you load the application it will ask you to allow location services to use your geolocation to pinpoint you on a map and provide the forecast for the next 5 days in your area. After creating an account and logging in you are able to create new garage sale listings, and view the listings around you. 
+        <p> SellYoStuff is a work that was created with two classmates, Tyler B and Zach. 
+          <br />When you load the application it will ask you to allow location services to use your geolocation to pinpoint you on a map and provide the forecast for the next 5 days in your area. 
+          <br />After creating an account and logging in you are able to create new garage sale listings, and view the listings around you. 
+        </p>
+   
+        <img src={passwordgenerator} className="Password-Generator portfolioImg" alt="passwordgen" />
+      {/* Place Image HERE */}
+        <p> PLEASE DESCRIBE PROJECT HERE!!!
+        </p>
+        <img src={weatherdashboard} className="Weatherdashbaord portfolioImg" alt="weather" />
+      {/* Place Image HERE */}
+        <p> PLEASE DESCRIBE PROJECT HERE!!!
         </p>
     </div>    
 
   
-         <footer class="footer text-center">
-        <div class="container">
-          <div class="row">
+
     
-            
-            <div class="col-lg-4 mb-5 mb-lg-0">
-              <h4 class="text-uppercase mb-4">Location</h4>
-              <p class="lead mb-0"> Aurora, CO 80015</p>
-            </div>
+
     
-            <div class="col-lg-4 mb-5 mb-lg-0">
-              <h4 class="text-uppercase mb-4">Around the Web</h4>
-              <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/Samanth25431418" target="_blank">
-                <i class="fab fa-fw fa-twitter"></i>
-              </a>
-              <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/in/samantha-scott1992/" target="_blank">
-                <i class="fab fa-fw fa-linkedin-in"></i>
-              </a>
-              <a class="btn btn-outline-light btn-social mx-1 " href="https://github.com/samanthascott1992" target="_blank" title="GitHub">
-                <i class="fab fa-fw fa-github"></i>
-              </a>
-             </div>
-    
-          </div>
-        </div>
-      </footer>
+
  );
 }
-
 export default App;
+
+
