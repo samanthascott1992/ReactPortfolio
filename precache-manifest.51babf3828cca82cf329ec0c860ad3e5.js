4b948c68a24d3af816a2f52d5e01330083ea8500
Samantha Scott <samanthascott1992@gmail.com>
2020-07-08 09:23:13 -0600

    Updates

---
+++ precache-manifest.51babf3828cca82cf329ec0c860ad3e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c480a5df1d0069df594c0e2d201de66",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "7803c40251edd3458102",
+    "url": "/React-Portfolio/static/css/main.dfbd0e75.chunk.css"
+  },
+  {
+    "revision": "8b8036ac5e4cdb895fae",
+    "url": "/React-Portfolio/static/js/2.2cbbcfaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.2cbbcfaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7803c40251edd3458102",
+    "url": "/React-Portfolio/static/js/main.f2c4c4a3.chunk.js"
+  },
+  {
+    "revision": "c3d7c804f21d34cc577a",
+    "url": "/React-Portfolio/static/js/runtime-main.ad880b05.js"
+  },
+  {
+    "revision": "e68427c9cf82c0da546ac3021b6e50ad",
+    "url": "/React-Portfolio/static/media/BoozyFood.e68427c9.PNG"
+  },
+  {
+    "revision": "91932b6cf351693607b06cab396f7fe0",
+    "url": "/React-Portfolio/static/media/PasswordGenerator.91932b6c.PNG"
+  },
+  {
+    "revision": "ab9d9df7e4dbb2777e7fc015b96d4fcc",
+    "url": "/React-Portfolio/static/media/cohortcorner.ab9d9df7.png"
+  },
+  {
+    "revision": "038c7dec471812f9c1f6adbc84f694f6",
+    "url": "/React-Portfolio/static/media/sellyostuff.038c7dec.PNG"
+  },
+  {
+    "revision": "c072a57566143bfee381d264db8a869a",
+    "url": "/React-Portfolio/static/media/weatherdashboard.c072a575.PNG"
+  }
+]);
