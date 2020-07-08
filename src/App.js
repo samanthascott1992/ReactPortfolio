79ebed67e0116a1a2a134a6243240abae02f57aa
Samantha Scott <samanthascott1992@gmail.com>
2020-07-08 09:37:37 -0600

    added links for repos

--- src/App.js
+++ src/App.js
@@ -45,22 +45,35 @@ function App() {
       {/* Place Image HERE */}
         <p>BoozyFood was a collaborative effort with two other classmates. When you search for an ingredient, then up to five recipes generate. 
           <br />When you click the Random Drink! button, you will be presented with a drink image, ingredients, and instructions for how to make it. 
-        
+          <br />
+                <a href="https://samanthascott1992.github.io/project1/" target="_blank">Active BoozyFood Page </a> <br />
+                <a href="https://github.com/samanthascott1992/project1.git" target="_blank">GitHub Repository Page</a>
         </p>
         <img src={sellThings} className="Project-Two portfolioImg" alt="project2" />
       {/* Place Image HERE */}
         <p> SellYoStuff is a work that was created with two classmates, Tyler B and Zach. 
           <br />When you load the application it will ask you to allow location services to use your geolocation to pinpoint you on a map and provide the forecast for the next 5 days in your area. 
           <br />After creating an account and logging in you are able to create new garage sale listings, and view the listings around you. 
+          <br />
+                <a href="https://mysterious-eyrie-67445.herokuapp.com/" target="_blank">Active Application</a> <br />
+                <a href="https://github.com/samanthascott1992/Project2.git" target="_blank">GitHub Repository Page</a>
         </p>
    
         <img src={passwordgenerator} className="Password-Generator portfolioImg" alt="passwordgen" />
       {/* Place Image HERE */}
-        <p> PLEASE DESCRIBE PROJECT HERE!!!
+        <p> This application allows you to generate a random password that has between 8 & 128 characters 
+          <br />and contains some or all of the following : upper case, lower case, special characters, and/or numbers.
+          <br />
+                <a href="https://samanthascott1992.github.io/Password-Generator/" target="_blank">Active Password Generator Page</a> <br />
+                <a href="https://github.com/samanthascott1992/Password-Generator.git" target="_blank">GitHub Repository Page</a>
         </p>
         <img src={weatherdashboard} className="Weatherdashbaord portfolioImg" alt="weather" />
       {/* Place Image HERE */}
-        <p> PLEASE DESCRIBE PROJECT HERE!!!
+        <p> Prior tio scheduling your next trip do not forget to look at the weather in the city you are going to!
+          <br /> Using the Weather Dashboard, you can search for any city and get the current and upcoming weather.
+          <br />
+                <a href="https://samanthascott1992.github.io/Weather-Dashboard/" target="_blank">Active Weather Dashboard Page</a> <br />
+                <a href="https://github.com/samanthascott1992/Weather-Dashboard.git" target="_blank">GitHub Repository Page</a>
         </p>
     </div>    
 
